50317c4655d83ad877a981e2fc63635edf69e9cb
Priyanshu Thakar <101591861+priyanshuthakar24@users.noreply.github.com>
2023-07-22 01:15:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-
 import Searchbar from "./Components/Searchbar";
 import store from "./State/store";
 import Navigation from "./Navigation";
